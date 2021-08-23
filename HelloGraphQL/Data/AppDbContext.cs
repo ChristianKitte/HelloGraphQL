846dbfab6abdab9cb28e6f238545e5be0d08b45b
Christian Kitte <chkitte@web.de>
2021-08-23 22:55:53 +0200

    Inital Code

---
+++ HelloGraphQL/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HelloGraphQL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloGraphQL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<LinkGroup> LinkGroups { get; set; }
+    }
+}
---
+++ HelloGraphQL/Models/LinkGroup.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelloGraphQL.Models
+{
+    public class LinkGroup
+    {
+        [Key] public int LinkGroup_ID { get; set; }
+        [Required] public string LinkGroup_Name { get; set; }
+        public string LinkGroup_Description { get; set; }
+    }
+}
