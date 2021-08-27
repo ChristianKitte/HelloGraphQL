a260ba15b25389353aeed31b19878ff5f96602a0
Christian Kitte <chkitte@web.de>
2021-08-27 21:46:43 +0200

    Add Description Sample

--- HelloGraphQL/Models/LinkGroup.cs
+++ HelloGraphQL/Models/LinkGroup.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
+using HotChocolate;
 
 namespace HelloGraphQL.Models
 {
+    [GraphQLDescription("Stellt eine Gruppe von Links dar")]
     public class LinkGroup
     {
         [Key] public int LinkGroup_ID { get; set; }
-        [Required] public string LinkGroup_Name { get; set; }
+
+        [GraphQLDescription("Der Name eine Gruppe von Links")]
+        [Required]
+        public string LinkGroup_Name { get; set; }
+
+        [GraphQLDescription("Beschreibt den Inhalt der Gruppe")]
         public string LinkGroup_Description { get; set; }
+
         public ICollection<Link> LinkGroup_Links { get; set; } = new List<Link>();
     }
 }
